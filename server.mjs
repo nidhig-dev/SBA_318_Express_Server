5be400cc7788927e7f17100e30dcaf5109b78625
nidhig-dev <goyal.nidhi@gmail.com>
2025-09-06 16:38:54 -0400

    Basic Server running set up

---
+++ server.mjs
@@ -0,0 +1,29 @@
+//Imports
+import express from "express"
+
+//env set up
+const app= express();
+const PORT=3000;
+
+//middleware
+app.use(express.json());
+
+//routes
+
+
+
+//global error handling
+
+app.use(function(err,req,res,next){
+    res.status(err.status||500).json({msg:err.message})
+})
+
+//Incorrect path error handler
+app.use((req,res,next)=>{
+    res.status(404).json({msg:"Incorrect path entered"});
+})
+
+//Server listener
+app.listen(PORT,(req,res)=>{
+    console.log(`server running in port ${PORT}`);
+})
